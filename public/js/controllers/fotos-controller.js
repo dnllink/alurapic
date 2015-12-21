baa06ed588e555da3f4b7dffc682a1890ab0e8ae
Daniel Augusto Bonaldo <dabonaldo@stefanini.com>
2015-12-21 11:28:22 -0200

    commit inicial

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,12 @@
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+
+    $scope.fotos = [];
+
+    $http.get('/v1/fotos')
+        .then(function (retorno) {
+            $scope.fotos = retorno.data;
+        }).catch(function (erro) {
+            console.log(erro);
+        });
+
+});
