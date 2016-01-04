3be296df98522451d8b6b59ce2f18470b4fb03fc
Daniel Augusto Bonaldo <dabonaldo@stefanini.com>
2016-01-04 14:30:03 -0200

    implementacao de delecao e alteracao das fotos

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,12 +2,31 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 
     $scope.fotos = [];
     $scope.filtro = '';
+    $scope.mensagem = '';
 
-    $http.get('/v1/fotos')
-        .then(function (retorno) {
-            $scope.fotos = retorno.data;
-        }).catch(function (erro) {
-            console.log(erro);
-        });
+    function listarFotos() {
+        $http.get('/v1/fotos')
+            .then(function (retorno) {
+                $scope.fotos = retorno.data;
+            }).catch(function (erro) {
+                console.log(erro);
+            });
+    };
+
+    listarFotos();
+
+    $scope.remover = function (foto) {
+
+        $http.delete('/v1/fotos/' + foto._id)
+            .success(function () {
+                var indiceFoto = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(indiceFoto, 1);
+                $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+            })
+            .error(function (erro) {
+                $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+            });
+
+    };
 
 });
