46206f31b3599d84d34e69a1a5a6e55adb776144
Daniel Augusto Bonaldo <dabonaldo@stefanini.com>
2016-01-05 14:13:21 -0200

    adicionados os grupos de fotos com combobox

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -24,4 +24,19 @@ angular.module('minhasDiretivas', []).directive('meuPainel', function () {
 
     return ddo;
 
+}).directive('meuBotaoPerigo', function () {
+
+    var ddo = {};
+
+    ddo.restrict = 'E';
+
+    ddo.scope = {
+        nome: '@',
+        acao: '&'
+    };
+
+    ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
+
+    return ddo;
+
 });
